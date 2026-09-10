9bd1352f6cefa9fad2eb2f6159a7d4d190cbbec1
phansiriphat_w <phansiriphat_w@silpakorn.edu>
2026-09-10 05:24:29 +0000

    new data and state

--- mp-app/src/pages/Home.jsx
+++ mp-app/src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+import { movies } from "../data/data";
+import MovieCard from "../components/MovieCard";
+import MovieGallery from "../components/MovieGallery";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
-    </div>
+    <h2 className="mt-6 text-2xl font-bold text-slate-800">หนังแนะนำ</h2>
+      <div className = "mx-auto mt-8 max-w-5xl">
+        {movies.slice(0, 3).map((movie) => (
+          <MovieCard key={movie.id} title={movie.title} year={movie.year}/>
+        ))}
+      </div>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800">ดูหนังเพิ่มเติม</h2>
+      <div className="mx-auto mt-8 max-w-md">
+        <MovieGallery />
+      </div>
+    </div>  
   );
 }
 
